3b6446335e069ccf1a7fccb17761be838789c9b8
Michal Deak <mdeak@pixelfederation.com>
2017-11-26 13:19:21 +0100

    Still working on support for sounds...

--- src/Game.hx
+++ src/Game.hx
@@ -2,6 +2,8 @@
  *	Copyright 11/25/2017 Michal Deak. All rights reserved.
  */
 package ;
+
+import core.sound.SoundManager;
 import core.logger.Logger;
 import com.genome2d.components.renderable.GSprite;
 import com.genome2d.Genome2D;
@@ -11,14 +13,28 @@ import com.genome2d.textures.GTextureManager;
 class Game {
 
     public function new() {
+        testTexture();
+        testSound();
+    }
 
+    private function testTexture():Void{
+        Logger.log("TestTexture started...");
+
+        //Texture test
         var sprite:GSprite = GNode.createWithComponent(GSprite);
         //sprite.texture = GTextureManager.getTexture("test");
         sprite.texture = GTextureManager.getTextureAtlas("test_atlas").getSubTexture("test_atlas_1");
         sprite.node.setPosition(20, 20);
-
         Genome2D.getInstance().root.addChild(sprite.node);
 
-        Logger.log("Game started...");
+        Logger.log("TestTexture ended.");
+    }
+
+    private function testSound():Void{
+        Logger.log("TestSound started...");
+
+        SoundManager.test();
+
+        Logger.log("TestSound ended.");
     }
 }
--- src/core/sound/SoundManager.hx
+++ src/core/sound/SoundManager.hx
@@ -2,9 +2,24 @@
  *	Copyright 11/26/2017 Michal Deak. All rights reserved.
  */
 package core.sound;
+#if js
+import howler.Howl;
+#end
 class SoundManager {
     public function new() {
     }
 
     //TODO
+
+    static public function test(){
+        #if js
+        //Sound test - ONLY JS
+        var options:HowlOptions = {};
+        options.src = ["assets/sounds/test_sound.mp3"];
+        options.autoplay = false;
+        options.loop = true;
+        var snd:Howl = new Howl(options);
+        snd.play();
+        #end
+    }
 }
